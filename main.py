1d2fa943c6a0939bc41c484762c560dc89f86d10
OBY4300 <orhanberkyalnizoglu123@gmail.com>
2024-09-14 22:17:58 +0300

    Güncelleme main.py
    Gençler bizi dinlesin atıklarını azaltıcaklarsa.

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ from discord.ext import commands, tasks
 from itertools import cycle 
 from dotenv import load_dotenv
 import os
+import random 
 
-bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+#Hedef Kitle 1:
+#-------------------------
+#Evde ürettikleri atık miktarını azaltmak isteyen ancak nereden başlayacaklarını bilmeyen gençler
+#-------------------------
+
+client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 bot_status = cycle(["1", "2", "3", "4"])
 
@@ -16,24 +22,28 @@ SPEELS = []
 
 @tasks.loop(seconds=5)
 async def change_status():
-    await bot.change_presence(activity=discord.Game(next(bot_status)))
+    await client.change_presence(activity=discord.Game(next(bot_status)))
 
-@bot.event
+@client.event
 async def on_ready():
-    channel = bot.get_channel(1007625208443703366)
+    channel = client.get_channel(1007625208443703366)
     await channel.send("Yor'ue Bluetooth Device Is Connected Successfully!")
     change_status.start()
 
     try:
-        synced_commands = await bot.tree.sync()
+        synced_commands = await client.tree.sync()
         print(f"Synced {len(synced_commands)} commands.")
     except Exception as e:
         print("An error with syncing application commands has occurred", e)
 
-@bot.tree.command(name="Clean", description="You can fight against your friend or something")
-async def pvp(interaction: discord.Interaction, player2: discord.Member):
-    channel = bot.get_channel(interaction.channel.id)
+@client.command()
+async def Atik(ctx):
+    tavsiyeler = ["Aynı zamanda salğıa zararlı olan fast food tüketimini azaltabilrsin"]
+    a = random.choice(tavsiyeler)
+    await ctx.send(a)
+    
 
     await interaction.response.send_message(f"You invited <@{player2.id}> to play.", ephemeral=True, view=))
 
-bot.run(os.getenv("TOKEN"))
+client.run(os.getenv("TOKEN"))
+
